687f29e5c1c2775914fef2063e5e0130505aa7b6
Mandeep2001 <39806321+Mandeep2001@users.noreply.github.com>
2019-10-14 22:11:22 +0200

    Refactor on boarding navigation

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -9,7 +9,9 @@ import LoadingScreen from '../screens/LoadingScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import SignupScreen from '../screens/SignupScreen';
 import SignupSecondScreen from '../screens/SignupSecondScreen';
-import LoginStack from './LoginStack';
+import LoginScreen from '../screens/LoginScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import InsertCodeScreen from '../screens/InsertCodeScreen';
 
 const AppTabBar = createBottomTabNavigator(
   {
@@ -35,11 +37,37 @@ const AppTabBar = createBottomTabNavigator(
 
 const AuthSwitch = createStackNavigator(
   {
-    Login: { screen: LoginStack, navigationOptions: { header: null } },
-    Signup: { screen: SignupScreen, navigationOptions: { header: null } },
-    CompleteSignup: { screen: SignupSecondScreen, navigationOptions: { header: null } },
+    Login: { screen: LoginScreen },
+    ForgotPassword: { screen: ForgotPasswordScreen },
+    ForgotPasswordCode: { screen: InsertCodeScreen },
+    Signup: { screen: SignupScreen },
+    CompleteSignup: { screen: SignupSecondScreen },
   },
-  { initialRouteName: 'Signup' }
+  {
+    initialRouteName: 'Signup',
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    },
+    transitionConfig: () => ({
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
+
+        const translateX = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [layout.initWidth, 0, 0],
+        });
+
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index, index + 0.99, index + 1],
+          outputRange: [0, 1, 1, 0.3, 0],
+        });
+
+        return { opacity, transform: [{ translateX }] };
+      },
+    }),
+  }
 );
 
 export default createAppContainer(
